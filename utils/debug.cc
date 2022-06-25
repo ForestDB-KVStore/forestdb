f45310478f6f663d7a970342cedadb955a13246c
Jung-Sang Ahn <jungsang.ahn@gmail.com>
2022-06-25 13:42:26 -0700

    Do not use custom signal handlers on ARM

--- utils/debug.cc
+++ utils/debug.cc
@@ -69,7 +69,8 @@ static void _dbg_hang_process(void)
 
 // to profile first install perf
 // echo 0 > /proc/sys/kernel/kptr_restrict
-#if defined(__linux__) && !defined(__ANDROID__)
+#if 1 && defined(__linux__) && !defined(__ANDROID__) \
+      && !defined(__arm__) && !defined(__aarch64__)
 #include <string.h>
 #include <dlfcn.h>
 #include <ucontext.h>
